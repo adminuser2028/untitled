0f42c2ec4a2f9a52b083acdbf2408e8d5cd2b510
大小姐会为我心动嘛 <@>
2022-09-16 19:35:13 +0800

    thrrdgit

--- src/main/java/git/twogit.java
+++ src/main/java/git/twogit.java
@@ -4,5 +4,7 @@ public class twogit {
     public static void main(String[] args) {
         System.out.println("hello ");
         System.out.println("我艹");
+        System.out.println("好好学习天天向上");
+
     }
 }
