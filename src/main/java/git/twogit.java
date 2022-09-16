d19ad3601d4cec76c0e929abe5cfdb333297541f
大小姐会为我心动嘛 <@>
2022-09-16 19:55:19 +0800

    hot-fixgit1

--- src/main/java/git/twogit.java
+++ src/main/java/git/twogit.java
@@ -6,6 +6,7 @@ public class twogit {
         System.out.println("我艹");
         System.out.println("好好学习天天向上");
         System.out.println("我李鸿祥就知道天天打管");
+        System.out.println("我李鸿祥天天打管");
 
     }
 }
