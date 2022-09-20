0a26057a6b08c44cf9f2f43c5ad7b5be044a5c7d
大小姐会为我心动嘛 <@>
2022-09-20 08:47:14 +0800

    push-git1

--- src/main/java/git/twogit.java
+++ src/main/java/git/twogit.java
@@ -3,11 +3,7 @@ package git;
 public class twogit {
     public static void main(String[] args) {
         System.out.println("hello ");
-        System.out.println("我艹");
         System.out.println("好好学习天天向上");
-        System.out.println("我李鸿祥就知道天天打管");
-        System.out.println("我李鸿祥天天打管");
-        System.out.println("我姥症酿天天就知道打管");
         System.out.println("master test");
         System.out.println("hostfix test");
         System.out.println("我要push李鸿翔");
